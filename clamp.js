24cc6a4cdfa213e0dcc1fc4dfdf99acd9a21e86e
Travis McKinney <travis.r.mckinney@gmail.com>
2014-10-22 16:51:50 -0400

    browserify support

--- clamp.js
+++ clamp.js
@@ -10,6 +10,8 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
   } else {
     root.clamp = factory();
   }
